94cc4acebc5b7c8d3e2eefa474282821782bac5b
JeffMahoney1991 <161242960+JeffMahoney1991@users.noreply.github.com>
2026-08-15 13:27:25 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "GET") {
     return res.json({ ok: true, method: "GET" });
   }
@@ -8,5 +8,4 @@ module.exports = async function handler(req, res) {
   }
 
   return res.json({ ok: true, method: "POST", bodyType: typeof req.body });
-};  }
 }
